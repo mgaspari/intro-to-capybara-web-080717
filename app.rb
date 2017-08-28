c72bc46002a8945311c3af1f3fc745873eaf1601
mgaspari <marcoanthonygaspari@gmail.com>
2017-08-28 17:36:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 class Application < Sinatra::Base
   # Write your code here!
+  get "/" do
+    erb :index
+  end
 
+  get "/greet" do
+    @uName = req.params["user_name"]
+    erb :greet
+  end
 end
